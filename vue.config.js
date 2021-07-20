be778599cbff839fa7c4c74f4b21347b8f14affa
yijun <sandy3068@yahoo.com.tw>
2021-07-21 04:08:55 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   publicPath: '/hexschool_vue2021_w8/'
 }
+module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= 'Pinpin'
+        return args
+      })
+  }
+}
